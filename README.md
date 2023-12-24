87889903a3efcf8da9bcf597234dc5545eed4b44
Logan B <logangbentley@gmail.com>
2023-12-25 10:39:15 +1300

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# ![Mini Libraries Icon](/static/LibrariesIcon.svg) Mini Libraries
+Mini Libraries (better title WIP) is a web app for sharing books on a small scale (think friends, family). It acts as a way to keep track of who is borrowing what, and as a way to check out other member's personal book collections.
+
+This was originally my Final Project for [CS50x 2023](https://cs50.harvard.edu/x/2023/), but I thought it was a neat concept and have decided to keep working on it. The project is in a very unstable state at present, and I'm currently working on improving organisation and making sure everything is stable before I will make any updates.
+
+## How to run Mini Libraries
+1. Install [Python3](https://wiki.python.org/moin/BeginnersGuide/Download) and [Pip](https://pip.pypa.io/en/stable/installation/)
+2. Download the source code for this project.
+3. Run `pip install -r requirements.txt` to install dependencies
+4. Run `flask run` to start the app.
---
+++ app.py
@@ -0,0 +1,230 @@
+from __future__ import print_function
+from datetime import datetime
+import sys
+
+from cs50 import SQL
+from flask import Flask, render_template, redirect, request, session, url_for, make_response
+from flask_session import Session
+import requests
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from helpers import login_required
+
+app = Flask(__name__)
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+db = SQL("sqlite:///library.db")
+db.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username TEXT NOT NULL, hash TEXT NOT NULL)")
+db.execute("CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, owner_id INTEGER NOT NULL, olid TEXT, isbn TEXT, title TEXT, borrower_id INTEGER, FOREIGN KEY(owner_id) REFERENCES users(id))")
+db.execute("CREATE TABLE IF NOT EXISTS requests (book_id INTEGER UNIQUE NOT NULL, user_id INTEGER NOT NULL, request_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, time INTEGER);")
+
+@app.route("/")
+@login_required
+def index():
+    user = session["user_id"]
+    name = db.execute("SELECT username FROM users WHERE id = ?", user)[0][
+        "username"
+    ]
+    book_count = len(db.execute("SELECT id FROM books"))
+    user_count = len(db.execute("SELECT id FROM users"))
+    user_books = len(db.execute("SELECT id FROM books WHERE owner_id = ?", user))
+    user_takeouts = len(db.execute("SELECT request_id FROM requests WHERE user_id = ?", user))
+
+    requested_books = db.execute("SELECT books.id, books.isbn, books.title, users.username FROM books INNER JOIN requests ON books.id = requests.book_id INNER JOIN users ON requests.user_id = users.id WHERE books.owner_id = ? AND books.borrower_id IS NULL", user)
+    user_requests = db.execute("SELECT id, isbn, title FROM books WHERE borrower_id = ?", user)
+    return render_template("index.html",
+                           name=name, book_count=book_count, users=user_count, user_books=user_books, user_takeouts=user_takeouts, requested_books=requested_books, user_requests=user_requests
+                           )
+
+@app.route("/books")
+@login_required
+def books():
+    books = db.execute("SELECT id, isbn, title, borrower_id FROM books")
+
+    return render_template("books.html", books=books)
+
+@app.route("/api/book/add", methods=["POST"])
+@login_required
+def add_book():
+    owner_id = session["user_id"]
+    isbn = request.form.get("isbn")
+
+    if len(isbn) != 13 and len(isbn) != 10:
+        return "The ISBN provided is the wrong length (should be either 10 or 13)"
+
+    r = requests.get(f'http://openlibrary.org/api/volumes/brief/isbn/{isbn}.json')
+    book = r.json()["records"][list(r.json()["records"])[0]]
+    print(book, file=sys.stderr)
+    olid = book["olids"][0]
+    title = book["data"]["title"]
+
+    db.execute("INSERT INTO books (owner_id, olid, isbn, title) VALUES (?, ?, ?, ?)", owner_id, olid, isbn, title )
+
+    return redirect(url_for("books"))
+
+@app.route("/api/book/delete", methods=["POST"])
+@login_required
+def delete_book():
+    owner_id = session["user_id"]
+    book_id = request.form.get("book_id")
+
+    if len(db.execute("SELECT id FROM books WHERE id = ? AND owner_id = ?", book_id, owner_id)) != 0:
+        db.execute("DELETE FROM books WHERE id = ? AND owner_id = ?", book_id, owner_id)
+        db.execute("DELETE FROM requests WHERE book_id = ?", book_id)
+
+    return redirect(url_for("books"))
+
+@app.route("/api/book/request", methods=["POST"])
+@login_required
+def request_book():
+    values = {
+        "book_id": request.form.get("book_id"),
+        "user_id": session["user_id"],
+        "time": datetime.now()
+    }
+
+    db.execute("INSERT INTO requests (book_id, user_id, time) VALUES (?, ?, ?)", *values.values())
+    return values
+
+@app.route("/api/book/cancel_request", methods=["POST"])
+@login_required
+def cancel_request():
+    book_id = request.form.get("book_id")
+    db.execute("DELETE FROM requests WHERE user_id = ? AND book_id = ?", session["user_id"], book_id)
+    return redirect(url_for("book_details", book_id=book_id))
+
+@app.route("/api/book/fulfill_request", methods=["POST"])
+@login_required
+def fulfill_request():
+    book_id = request.form.get("book_id")
+    user_id = session["user_id"]
+    db.execute("DELETE FROM requests WHERE user_id = ? AND book_id = ?", user_id, book_id)
+    db.execute("UPDATE books SET borrower_id = ? WHERE id = ?", user_id, book_id)
+    return make_response(None, 204)
+
+@app.route("/api/book/return", methods=["POST"])
+@login_required
+def return_book():
+    db.execute("UPDATE books SET borrower_id = NULL WHERE id = ? AND borrower_id = ?", request.form.get("book_id"), session["user_id"])
+    return redirect("/")
+
+@app.route("/books/<string:book_id>")
+@login_required
+def book_details(book_id):
+    details = db.execute("SELECT * FROM books WHERE id = ?", book_id)
+    if not details:
+        return redirect("/books")
+    details = details[0]
+    request = db.execute("SELECT request_id FROM requests WHERE book_id = ?", book_id)
+    has_book = details["borrower_id"] == session["user_id"]
+    is_owner = details["owner_id"] == session["user_id"]
+
+    olid = details["olid"]
+
+    r = requests.get(f'http://openlibrary.org/books/{olid}.json')
+    book = r.json()
+    work_id = book["works"][0]["key"]
+
+    work = requests.get(f'http://openlibrary.org{work_id}.json').json()
+    desc = work.get("description")
+    text = []
+    if desc:
+        desc = desc["value"]
+        text = desc.split('\n')
+
+    return render_template("book_details.html", id=book_id, olid=olid, title=details["title"], subtitle=book.get("subtitle"), description=text, request=request, has_book=has_book, is_owner=is_owner)
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        # Return a version of the page with an error message
+        def error(error):
+            nonlocal username
+            nonlocal password
+            return render_template("register.html", username=username, password=password, error=error)
+
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+
+        if not username:
+            return error("Empty username")
+
+        if not password:
+            return error("Empty password")
+
+        if len(db.execute("SELECT id FROM users WHERE username = ?", username)) != 0:
+            return error("Username already in use")
+
+        if password != confirmation:
+            return error("Password and confirmation do not match")
+
+        hash = generate_password_hash(password)
+        db.execute(
+            "INSERT INTO users (username, hash) VALUES (?, ?)", username, hash
+        )
+
+        return redirect("/")
+
+    else:
+        return render_template("register.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Return a version of the page with an error message
+        def error(error):
+            nonlocal username
+            nonlocal password
+            return render_template("login.html", username=username, password=password, error=error)
+
+        username = request.form.get("username")
+        password = request.form.get("password")
+        # Ensure username was submitted
+        if not username:
+            return error("must provide username")
+
+        # Ensure password was submitted
+        elif not password:
+            return error("must provide password")
+
+        # Query database for username
+        rows = db.execute(
+            "SELECT * FROM users WHERE username = ?", username
+        )
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(
+            rows[0]["hash"], password
+        ):
+            return error("invalid username and/or password")
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
